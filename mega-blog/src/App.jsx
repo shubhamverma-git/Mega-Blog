929977eea51652b3289a490d780779ac6497abd5
Shubham Verma <vermashubham2707@gmail.com>
2023-12-21 01:16:11 +0530

    -- created environment variable (env)

--- mega-blog/src/App.jsx
+++ mega-blog/src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 
 function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL); // method to take access of env file
   return (
     <>
       <h1>Mega Blog App with Appwrite</h1>
